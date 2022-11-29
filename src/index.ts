591b878fdf39e1e25c4d0b8d557c7a0ff5590dc5
dylan <dylanc.codcazeraz>
2022-11-29 16:32:05 +0100

    :lipstick: Create Colors class

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import readline from "readline"
+import { Colors } from "./utils/Colors.js";
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+
+rl.question(Colors.FgCyan + " What's your name ?\n", (res) => {
+  console.log("Salut " + res);
+  rl.close()
+})
